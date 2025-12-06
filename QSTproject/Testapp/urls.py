b2ce19062c942e0e1ee51c624913fd1342cdc192
HritikGiri2005 <hritikgiri2@gmail.com>
2025-12-06 13:57:04 +0530

    template and about page created

--- QSTproject/Testapp/urls.py
+++ QSTproject/Testapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from Testapp import views
 
 urlpatterns = [
-    path('hello/',views.template_view)
+    path('hello/',views.template_view),
+    path('about/',views.about),
 ]
--- QSTproject/Testapp/views.py
+++ QSTproject/Testapp/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def template_view(request):
-    return render(request,'home.html')
+    name = 'hritik'
+    marks = 100
+    rollno = 111
+    my_dic = {'uname':name,'umarks':marks,'uroll':rollno}
+    return render(request,'home.html',context=my_dic)
+    
+
+def about(request):
+    return render(request,"about.html")
