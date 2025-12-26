4e439339949501c4ff545cdceeffbba4973df4fd
HritikGiri2005 <hritikgiri2@gmail.com>
2025-12-26 12:17:56 +0530

    created models and registered in admin

--- modelsProject/testModels/admin.py
+++ modelsProject/testModels/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','enrollment_data')
+    search_fields = ('first_name','last_name')
+    list_filter = ('enrollment_date')
+
+admin.site.register(Student,StudentAdmin)
