6c457bc1ef96b97a6661284319752a9e2f853a4a
HritikGiri2005 <hritikgiri2@gmail.com>
2025-12-26 13:03:55 +0530

    changed admin.py

--- modelsProject/testModels/admin.py
+++ modelsProject/testModels/admin.py
@@ -3,7 +3,7 @@ from .models import Student
 
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name','last_name','enrollment_data')
+    list_display = ('first_name','last_name','enrollment_date')
     search_fields = ('first_name','last_name')
     list_filter = ('enrollment_date')
 
