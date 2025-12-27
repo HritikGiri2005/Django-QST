a16981a79c8544c110e6613e25c760f6fb0c77a2
HritikGiri2005 <hritikgiri2@gmail.com>
2025-12-27 11:41:54 +0530

    created template studentForm.html

--- ModelFormProject/testapp/views.py
+++ ModelFormProject/testapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from testapp.forms import StudentForm
 # Create your views here.
+
+def StudentForm(request):
+    form = StudentForm()
+    return render(request,'testapp/studentForm.html')
