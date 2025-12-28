bf31f82229ee117b65e0c3a7d4f88a57a83e11bd
HritikGiri2005 <hritikgiri2@gmail.com>
2025-12-28 16:52:38 +0530

    created models for project1

--- Sampleproject1/testapp/models.py
+++ Sampleproject1/testapp/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class userProfile(models.Model):
+class UserProfile(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    about = models.TextField()
+    gender = models.CharField(max_length=100)
+    skills = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profiles/')
+    dob = models.DateField()
+
+    def __str__(self):
+        return self.username
     
+
