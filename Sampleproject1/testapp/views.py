7f6b9fc483534566a3a04b8e693fbb0cd0f70d82
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-05 16:59:47 +0530

    changed views.py and wrote register.html correctly

--- Sampleproject1/testapp/views.py
+++ Sampleproject1/testapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import UserProfile
+from .forms import UserProfile
 
 # Create your views here.
 
 def register(request):
-    return render(request,'testapp/register.html')
+    form = UserProfile()
+    return render(request, 'testapp/register.html', {'form': form})
 
 def success(request):
     return render(request,'testapp/success.html')
