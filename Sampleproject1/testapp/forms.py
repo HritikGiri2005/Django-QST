2c811a04700e6973e6b38eb0d2a131014b9d55bf
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-05 13:10:11 +0530

    changed forms.py to advance form

--- Sampleproject1/testapp/forms.py
+++ Sampleproject1/testapp/forms.py
@@ -1,7 +1,45 @@
 from django import forms
-from testapp.models import UserProfile
+from .models import UserProfile
 
-class UserForm(forms.ModelForm):
+class UserProfileForm(forms.ModelForm):
+
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    ]
+
+    gender = forms.ChoiceField(
+        choices=GENDER_CHOICES,
+        widget=forms.RadioSelect
+    )
+
+    profile_pic = forms.ImageField(
+        label='Upload Profile Picture',
+        widget=forms.ClearableFileInput(attrs={
+            'accept': 'image/*'
+        })
+    )
+
     class Meta:
-        model=UserProfile
-        fields = '__all__'
+        model = UserProfile
+        fields = "__all__"
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Enter username'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Enter password'
+            }),
+            'about': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Tell something about yourself'
+            }),
+            'skills': forms.TextInput(attrs={
+                'placeholder': 'e.g. Python, Django'
+            }),
+            'dob': forms.DateInput(attrs={
+                'type': 'date'
+            }),
+        }
