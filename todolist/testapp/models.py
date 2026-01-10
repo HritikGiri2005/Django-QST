019d88681f65dcd7197b8de51418ed1d80de1fb6
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-10 12:14:48 +0530

    created models.py

--- todolist/testapp/models.py
+++ todolist/testapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class todo(models.Model):
+    task_name = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
