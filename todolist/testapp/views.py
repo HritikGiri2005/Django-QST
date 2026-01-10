4d8344df9f2e52e3c882e47578cd348e70003f2e
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-10 12:35:48 +0530

    made style.css

--- todolist/testapp/views.py
+++ todolist/testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import todo
+from django.views.generic import CreateView,DeleteView,UpdateView,ListView
 
 # Create your views here.
+class AddTask(CreateView):
+    model = todo
+    template_name = "create_task.html"
