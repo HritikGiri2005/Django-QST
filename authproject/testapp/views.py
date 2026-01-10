332015872bdbaf843f9af594b3ce808d4f276bdd
HritikGiri2005 <hritikgiri2@gmail.com>
2026-01-10 14:26:09 +0530

    rendered navbar

--- authproject/testapp/views.py
+++ authproject/testapp/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views.generic import View
 # Create your views here.
 
-def view(request):
-    return render("navbar.html",name="navbar")
+def nav(request):
+    return render(request,"navbar.html")
 
