8d5ad742e9fffb2566abc426862350946fdfad0e
HritikGiri2005 <hritikgiri2@gmail.com>
2026-02-07 12:58:07 +0530

    working onetomany

--- ModelRelation/testapp/admin.py
+++ ModelRelation/testapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Person,AadharCard
+from .models import Person,AadharCard,Father,Children
 
 # Register your models here.
 
@@ -9,8 +9,13 @@ class PersonModel(admin.ModelAdmin):
 class AadharModel(admin.ModelAdmin):
     list_display = ('person','number')
 
-class AadharModel(admin.ModelAdmin):
-    list_display = ('person','number')
-    
+class FatherModel(admin.ModelAdmin):
+    list_display = ('name',)
+
+class ChildrenModel(admin.ModelAdmin):
+    list_display = ('father','child_name')
+
 admin.site.register(Person,PersonModel)
 admin.site.register(AadharCard,AadharModel)
+admin.site.register(Father,FatherModel)
+admin.site.register(Children,ChildrenModel)
